db5499b74eb099f567ed28d42f4b05c08b7f3e3a
Vanja <69113324+vanjadu@users.noreply.github.com>
2023-05-05 19:18:20 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,6 +7,14 @@ const nextConfig = {
   },
   images: {
     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   },
 }
 
